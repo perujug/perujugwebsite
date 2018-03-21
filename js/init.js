e5a9b3a374c562f4489219fe22ca498158a71d5c
Ytalo Borja Mori <ytaloborjam@gmail.com>
2018-03-21 16:17:01 -0500

    Adding the main photo with parallax

--- js/init.js
+++ js/init.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.carousel').carousel();
-
+    $('.parallax').parallax();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
